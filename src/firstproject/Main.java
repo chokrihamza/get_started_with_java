eeb006ba6251f5042266a94c9f044b1d6b78d114
chokrihamza <chokrihamza1994@gmail.com>
2021-08-26 11:24:02 +0200

    work with java preliminary

---
+++ src/firstproject/Main.java
@@ -0,0 +1,33 @@
+package firstproject;
+
+public class Main {
+
+	  public static void main(String[] args) {
+		   int num=10;
+		   float number_float=10.0f;
+		   System.out.println("the value of the number is :");
+		   System.out.println(num+number_float);
+		   var number=10;
+		   var number1="hello";
+		   var number2=20;
+		   double value=20.0d;
+		   // make addition and then make concatination
+		   System.out.println(number+number2+number1);
+		   //make concatination and then addition 
+		   System.out.println(number1+number+number2); 	
+		   // both of then 
+		   System.out.println(number1+(number+number2));
+		   System.out.println(value);
+		   System.out.println(Double.MAX_VALUE);
+		   System.out.println(Double.MIN_VALUE);
+		   System.out.println(Integer.MAX_VALUE);
+		   System.out.println(Integer.MIN_VALUE);
+		   System.out.println(Byte.MAX_VALUE);
+		   System.out.println(Byte.MIN_VALUE);
+		   System.out.println(Character.MAX_VALUE);
+		   System.out.println(Character.MIN_VALUE);
+		   System.out.println(Double.valueOf("2000"));
+		   
+		  }
+	
+}
